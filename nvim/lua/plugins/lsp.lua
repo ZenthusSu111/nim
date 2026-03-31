b9d51566b88104c8fea2ac286b22bd60de2e5e87
iansu <4B1L0901@stust.edu.tw>
2026-03-31 09:34:48 +0800

    fix lazydev config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,6 +3,13 @@ return {
 		"lazydev.nvim",
 		ft = "lua",
 		dep_of = { "blink.cmp" },
+		after = function()
+			require("lazydev").setup({
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			})
+		end,
 	},
 	{
 		"rustaceanvim",
