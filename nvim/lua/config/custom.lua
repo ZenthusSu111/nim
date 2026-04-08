c34dbbd02d515f77af82e4fd03b270195a9f7311
iansu <4B1L0901@stust.edu.tw>
2026-04-08 13:31:27 +0800

    add 0.12 treesitter api

--- nvim/lua/config/custom.lua
+++ nvim/lua/config/custom.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd("ExitPre", {
 	command = "set guicursor=a:ver90",
 	desc = "Set cursor back to beam when leaving Neovim.",
 })
+
+-- 0.12 treesitter api
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	callback = function()
+-- 		-- 檢查是否有對應的 parser 存在
+-- 		if pcall(vim.treesitter.start) then
+-- 			-- 成功啟動內建高亮
+-- 		end
+-- 	end,
+-- })
